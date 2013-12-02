21e3ba45541ea8baa39c1f1bd9a0de21fd063e22
Michael Schiff <schiff.michael@gmail.com>
2013-12-01 21:25:55 -0800

    dragging is working, had to limit the escape iters to make it feasible to draw continuously.

--- fractals.js
+++ fractals.js
@@ -1,23 +1,35 @@
+//setup the canvas
 var canvas = document.getElementById("myCanvas");
 var c = canvas.getContext("2d");
 var imgData = c.createImageData(500, 500);
 var pixels = imgData.data;
 
+//setup the callbacks. need to know about canvas and and fractal fn
+var handlers = new callbacks(canvas, mandelbrot);
+canvas.addEventListener('mousedown', handlers.onClick);
+canvas.addEventListener('mousemove', handlers.onDrag);
+canvas.addEventListener('mouseup', handlers.onUp);
 
-draw(mandelbrot, 250, 0, 0);
+//set zoom and draw initial image
+var zoom = 1000;
+var center_x = -500;
+var center_y = 0;
+draw(mandelbrot, zoom, center_x, center_y);
 
+//draws the escape image of f, centered on (cx,cy), at zoom scale. 
 function draw(f, zoom, cx, cy) {
     for (var x = 0; x < 500; x++) {
 		var x0 = x-250;
         for (var y = 0; y < 500; y++) {
 		    var y0 = y-250;
             var iters = f((x0+cx)/zoom,(y0+cy)/zoom);
-            color(x,y, 0, iters, 0, 255);
+            color(x,y, 0, 255-iters, iters, 255);
         }
     }
     c.putImageData(imgData, 0,0);
 }
 
+//colors a pixel
 function color(x, y, r, g, b, a) {
     index = (x + y * 500) * 4;
     pixels[index+0] = r;
@@ -26,26 +38,12 @@ function color(x, y, r, g, b, a) {
     pixels[index+3] = a;
 }
 
-var callbacks = {
-	ox:null,
-	oy:null,
-	clicked: false,
-	onClick: function(canvas, event) {
-	//sets initial click value	
-	},
-	onDrag: function(canvas, event) {
-	//determines move and updates drawing with new center nudges
-	},
-	onUp: function(canvas, event) {
-	//stop stuff
-	}
-};
-
+//fractal fn
 function mandelbrot(px,py) {
     var x = 0;
     var y = 0;
     var iteration = 0;
-    var max_iteration = 1000;
+    var max_iteration = 255;
     while ( x*x + y*y < 2*2 && iteration < max_iteration ) {
         var xtemp = x*x - y*y + px;
         y = 2*x*y + py;
@@ -53,4 +51,34 @@ function mandelbrot(px,py) {
         iteration = iteration + 1;
     }
     return iteration;
+}
+
+//callbacks for dragging around the image
+function callbacks(canvas, fn) {
+	
+	this.ox = null;
+	this.oy = null;
+	this.cx = 0;
+	this.cy = 0;
+	this.clicked = false;
+	
+	this.onClick = function(event) {
+		this.clicked = true;
+		this.ox = event.clientX;
+		this.oy = event.clientY;
+	};
+	
+	this.onDrag = function(event) {
+		if (this.clicked) {
+			this.cx = event.clientX - this.ox;
+			this.cy = event.clientY - this.oy;
+			draw(fn, zoom, center_x-this.cx, center_y-this.cy);
+		}
+	};
+	
+	this.onUp = function(event) {
+		this.clicked = false;
+		center_x -= this.cx;
+		center_y -= this.cy;
+	};
 }
